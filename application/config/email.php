5a5d11ebaefa5c3cf356a9d8eefa61e5560657ae
Samartha K V <samarthakv29@gmail.com>
2026-01-11 22:29:17 -0500

    Config email

--- application/config/email.php
+++ application/config/email.php
@@ -5,17 +5,17 @@
 // @link https://codeigniter.com/user_guide/libraries/email.html
 
 $config['useragent'] = 'Easy!Appointments';
-$config['protocol'] = 'mail'; // or 'smtp'
+$config['protocol'] = 'smtp'; // or 'smtp'
 $config['mailtype'] = 'html'; // or 'text'
-// $config['smtp_debug'] = '0'; // or '1'
-// $config['smtp_auth'] = TRUE; //or FALSE for anonymous relay.
-// $config['smtp_host'] = '';
-// $config['smtp_user'] = '';
-// $config['smtp_pass'] = '';
-// $config['smtp_crypto'] = 'ssl'; // or 'tls'
-// $config['smtp_port'] = 25;
-// $config['from_name'] = '';
-// $config['from_address'] = '';
+$config['smtp_debug'] = '0'; // or '1'
+$config['smtp_auth'] = TRUE; //or FALSE for anonymous relay.
+$config['smtp_host'] = 'smtp.gmail.com';
+$config['smtp_user'] = 'sdpflashinfotech@gmail.com';
+$config['smtp_pass'] = getenv('EA_SMTP_PASSWORD') ?: '';
+$config['smtp_crypto'] = 'tls'; // or 'tls'
+$config['smtp_port'] = 587;
+$config['from_name'] = 'Easy!Appointments';
+$config['from_address'] = 'sdpflashinfotech@gmail.com';
 // $config['reply_to'] = '';
 $config['crlf'] = "\r\n";
 $config['newline'] = "\r\n";
